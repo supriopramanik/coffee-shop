c0d5458a6c1cdedfc0a9f3d4b57f2dd93d034b94
suprio <suprio01626926397@gmail.com>
2026-02-04 02:51:49 +0600

    update

--- Working/backend/src/routes/adminAuthRoutes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { adminLogin } from '../controllers/adminAuthController.js';
-
-const router = Router();
-
-router.post('/login', adminLogin);
-
-export default router;
